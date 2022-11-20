52e8d0a364c8fc3f355cd284c590fd5df9c6b544
epetela51 <57976052+epetela51@users.noreply.github.com>
2022-11-19 21:59:30 -0500

    Create dynamic table
    used ngFor to loop through and dynamically create table rows/columns and use index to create round number
    
    SlicePipe is used to start the loop at a specific point and end the loop at a specific point.  In this circumstance it is used to end the loop at a certain point/not use the entire array loop

--- src/app/round-input/round-input.component.ts
+++ src/app/round-input/round-input.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoundInputComponent implements OnInit {
 
-  constructor() { }
+  tableHeaders: string [] = [
+    'Round Number',
+    '18 Hole Score',
+    '9 Hole Score',
+    'Course Rating',
+    'Slope Rating',
+    'Score Differential'
+  ]
+
+  // used to loop through and generate rows & columns of the table with input type of number
+  columnsAndRows: string[] = [
+    'number',
+    'number',
+    'number',
+    'number',
+    'number'
+  ]
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
+
+  
 
 }
