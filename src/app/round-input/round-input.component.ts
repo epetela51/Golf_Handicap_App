3fa8da6e2affb3e35fbf245745e936924c2600a0
epetela51 <57976052+epetela51@users.noreply.github.com>
2023-02-17 22:37:32 -0500

    Add/update reactive form implementation
    Updated HTML & TS files to include reactive form implementation
    
    Includes validation along with dynamic validation
    
    More changes coming as I am continuing to test/include reactive form aspects

--- src/app/round-input/round-input.component.ts
+++ src/app/round-input/round-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UserHandicapRounds } from '../data/user-handicap-modal';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from "@angular/forms";
+// import { Round } from '../data/user-handicap-modal';
 
 @Component({
   selector: 'app-round-input',
@@ -8,49 +9,59 @@ import { UserHandicapRounds } from '../data/user-handicap-modal';
 })
 export class RoundInputComponent implements OnInit {
 
-  // used to keep the original data and helps prevent from lost data from user pressing back button, cancel btn, etc.
-  originalUserHandicapRounds: UserHandicapRounds = {
-    eighteenHoleScore: 0,
-    nineHoleScore: 0,
-    courseRating: 0,
-    slopeRating: 0,
-    scoreDifferential: 0
+  // defines form model. Template will bind to this root form model
+  roundForm: FormGroup;
+  roundTotal: number;
+  numberMessage: string;
+
+  // must set the type to 'any' for this property otherwise you get an error when trying to use setMessages function
+  validationMessages: any = {
+    required: 'Please enter a valid number'
   }
 
-  test: Array <any> = [
-    '18 Hole Score',
-    '9 Hole Score',
-    'Course Rating',
-    'Slope Rating',
-    'Score Differential'
-  ]
-
-  userHandicapRounds: UserHandicapRounds = { ...this.originalUserHandicapRounds };
-
-  handicapNumber: number;
-
-  constructor() { 
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    // this uses Angulars FormBuilder service to create a form group and corresponding form controls
+    // putting this in ngOnInit and providing a value gives these inputs a default value on page/form load
+    this.roundForm = this.fb.group({
+      // for a number, you can set 'null' as the default and that will leave the input field blank
+      // you could also set the default value to 0 and the input will default to showing 0
+      eighteenHoleScore: [null, [Validators.required, Validators.min(18)]],
+      nineHoleScore: [null, [Validators.required, Validators.min(9)]]
+    })
+
+    // display validation based on user input (only for 18 hole score)
+    const numberControl = this.roundForm.get('eighteenHoleScore');
+    numberControl?.valueChanges.subscribe(value => {
+      console.log(value);
+      this.setMessage(numberControl);
+    })
   };
 
-  addRowOnBtnClick() {
-    console.log('Add a row on click')
-  }
-
-  deleteRowOnBtnClick() {
-    console.log('Delete a row on click');
+  setMessage(c: AbstractControl): void {
+    this.numberMessage = '';
+    console.log(c);
+    if ((c.touched || c.dirty) && c.errors) {
+      this.numberMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 
   // will PROBABLY need to use this method to calculate the handicap and display it on the screen
   calculateHandicapBtnClick() {
-    console.log('button firing for calculation')
-    console.log(this.userHandicapRounds);
+    console.log(this.roundForm);
+    console.log(this.roundForm.get('eighteenHoleScore')?.value);
 
-    this.handicapNumber = this.userHandicapRounds.eighteenHoleScore + this.userHandicapRounds.nineHoleScore
+    // to get a value from a specific form control you need to use the get method on that FormGroup
+    // this will look for the form control in that specific form group >> this will be case sensitive
+    const eighteeenHoleScore = this.roundForm.get('eighteenHoleScore')?.value
+    const nineHoleScore = this.roundForm.get('nineHoleScore')?.value
+    this.roundTotal = eighteeenHoleScore + nineHoleScore
 
-    console.log(`Addition is: ${this.handicapNumber}`);
+    console.log(`The sum of scores is: ${this.roundTotal}`);
+
   }
 
 }
