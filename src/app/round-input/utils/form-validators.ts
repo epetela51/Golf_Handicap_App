1dbea431d79fe7c9a30be2f4381aa576d735a2dd
epetela51 <57976052+epetela51@users.noreply.github.com>
2025-05-15 21:40:58 -0400

    Updated max round score
    Set max round input to 200 otherwise throw an error and mark the form input as invalid

--- src/app/round-input/utils/form-validators.ts
+++ src/app/round-input/utils/form-validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 /**
  * Creates a custom validator for round input validation.
- * Ensures the score entered is not less than the minimum required score.
+ * Ensures the score entered is not less than the minimum required score and not greater than 200.
  * @param score - Minimum score required for the round
  * @returns Validator function that returns an error object if validation fails
  */
@@ -10,6 +10,9 @@ export function roundInputValidation(score: number): ValidatorFn {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
     if ((control.touched || control.dirty) && control.value < score) {
       return { invalidForm: true };
+    }
+    if ((control.touched || control.dirty) && control.value > 200) {
+      return { maxScoreExceeded: true };
     }
     return null;
   };
